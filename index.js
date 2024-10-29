106596b0a9638b2cb239ef5818db225b655782f5
kareli2005 <cotneqareli123@gmail.com>
2024-10-30 02:12:24 +0400

    First commit

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const dotenv = require('dotenv')
 const cors = require('cors')
+const authRoutes = require('./routes/authRoutes')
 
 dotenv.config()
 
@@ -11,9 +12,10 @@ app.use(express.json())
 // connect to db
 
 app.get('/', (req, res) => {
-  console.log('asdasdasd')
   res.send("hello world!")
 })
+
+app.use('/', authRoutes)
 
 const PORT = process.env.PORT || 5000
 app.listen(PORT, () => {
